5d32230b8617ea49a8de14b8ed870e99b1e24d24
Bulliuser <92454354+Bulliuser@users.noreply.github.com>
2022-11-09 11:37:21 +0100

    Skripte für Ölpreis

---
+++ pipeline2 GitHubProjekt Notebook.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import requests
+
+
+# In[2]:
+
+
+df=pd.read_excel("https://www.eia.gov/dnav/pet/hist_xls/RBRTEd.xls", sheet_name='Data 1', skiprows=2)
+
+
+# In[3]:
+
+
+df
+
+
+# In[4]:
+
+
+df.columns = ['Datum', 'Dollars pro Barrel']
+
+
+# In[5]:
+
+
+df
+
+
+# In[6]:
+
+
+df.info() # Datumsspalte checken, ob diese auch als Datum erkannt wird
+
+
+# In[7]:
+
+
+df=df.set_index('Datum')
+
+
+# In[8]:
+
+
+df_resampled=df.resample('M').mean() # wir berechnen den monatlichen Durchschnitt und benennen den Dataframe um
+
+
+# In[9]:
+
+
+df_resampled #Wir lassen uns den neuen Dataframe anzeigen
+
+
+# In[10]:
+
+
+df_resampled.plot.line()
+
+
+# In[11]:
+
+
+df_resampled.to_csv('oelpreis.csv')
+
+
+# In[12]:
+
+
+pip install jupyterthemes
+
+
+# In[ ]:
+
+
+
+
